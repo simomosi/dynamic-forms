0314e636e8957a39c7faf0020d402d3a11a296ec
simomosi <simomosi@users.noreply.github.com>
2021-03-08 22:47:03 +0100

    index.js transformed from closure to module

--- src/index.js
+++ src/index.js
@@ -1,46 +1,34 @@
 import DynamicForm from './DynamicForm.js';
 
 /** Module to easily manage dynamic forms */
-let dynamicForms = (function () {
-    'use strict';
 
-    /** @param {Map<String, DynamicForm>} formCollection a collection of DynamicForms instances */
-    let formCollection = new Map();
+/** @param {Map<String, DynamicForm>} formCollection a collection of DynamicForms instances */
+let formCollection = new Map();
 
-    /**
-     * Method to instantiate a single Dynamic Form
-     * @param {object} formConfiguration the form configuration
-     * @returns Instance of a Dynamic Form
-     */
-    function _makeForm(formConfiguration) {
-        let form = new DynamicForm(formConfiguration);
-        formCollection.set(form.getId(), form);
-        return form;
-    }
+/**
+ * Method to instantiate a single Dynamic Form
+ * @param {object} formConfiguration the form configuration
+ * @returns Instance of a Dynamic Form
+ */
+export function makeForm(formConfiguration) {
+    let form = new DynamicForm(formConfiguration);
+    formCollection.set(form.getId(), form);
+    return form;
+}
 
-    /**
-     * Method to retrieve a Dynamic Form instance.
-     * @param {int} id the form id
-     */
-    function _getForm(id) {
-        return formCollection.get(id);
-    }
+/**
+ * Method to retrieve a Dynamic Form instance.
+ * @param {int} id the form id
+ */
+export function getForm(id) {
+    return formCollection.get(id);
+}
 
-    /**
-     * Credits the author of Dynamic Forms
-     */
-    function _author() {
-        console.info('DynamicForms! Developed with \u2764 by simomosi - Ping me @ https://github.com/simomosi/dynamic-forms');
-    }
+/**
+ * Credits the author of Dynamic Forms, in hope to receive some love (and stars)
+ */
+function _author() {
+    console.info('DynamicForms! Developed with \u2764 by simomosi - Ping me @ https://github.com/simomosi/dynamic-forms');
+}
 
-    _author();
-
-    return {
-        makeForm: _makeForm,
-        getForm: _getForm,
-        author: _author,
-    }
-})();
-
-// export default dynamicForms;
-window.dynamicForms = dynamicForms; // Easier to use, makes this library available everywhere!
+_author();
