aa6cb0c45788aefc4eae2e7bbf149647c91db1dd
simomosi <37104045+simomosi@users.noreply.github.com>
2023-03-30 22:48:32 +0100

    Updated dependencies

--- dist/dynamicforms.js
+++ dist/dynamicforms.js
@@ -1,7 +1,7 @@
 /*!
  * 
  * Author: simomosi (https://github.com/simomosi)
- * Version: 1.1.1
+ * Version: 1.1.2
  * Repository: https://github.com/simomosi/dynamic-forms.git
  * Documentation: https://simomosi.github.io/dynamic-forms/
  *
