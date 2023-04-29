c5c8ff648b687b50e7d83d3e03946cd6240c3d40
simomosi <37104045+simomosi@users.noreply.github.com>
2023-04-29 14:47:18 +0100

    Changed file extension, added types to DynamicElement

--- src/DynamicElement.ts
+++ src/DynamicElement.ts
@@ -1,27 +1,30 @@
+import DynamicForm from './DynamicForm';
+import { FieldBehaviorConfiguration, FieldConfiguration, FieldFetchConfiguration, FieldIoConfiguration } from './FieldConfigurationTypes';
+
 /**
  * This class represents a Dynamic Element in a form.
  * In particular it is a generic input element, whose behavior is based on element.value property
  */
 class DynamicElement {
     /** @param {object} config - the element configuration */
-    config;
+    config: FieldConfiguration;
 
     /** @param {node | NodeList} htmlElement - the HTML Element returned by querySelectorAll. If more than one exists, the element is a NodeList */
-    htmlElement;
+    htmlElement: NodeList;
 
     /** @param {string} name - the element name */
-    name;
+    name: string;
 
     /** @param {object} io - object which groups some properties related to field input/output */
-    io;
+    io: FieldIoConfiguration;
 
     /** @param {object} fetch - object which groups some properties related to remote calls */
-    fetch; // todo maybe we can move this property to DynamicDropdown ?
+    fetch: FieldFetchConfiguration; // todo maybe we can move this property to DynamicDropdown ?
 
     /** @param {object} behavior - object which groups some properties related to field behavior */
-    behavior;
+    behavior: FieldBehaviorConfiguration;
 
-    /** @param {JSON} defaultConfig - property with default configuration values */
+    /** @param {object} defaultConfig - property with default configuration values */
     static defaultConfig = {
         'io': {
             'event': 'change',
@@ -30,11 +33,11 @@ class DynamicElement {
 
     /**
     * Class constructor
-    * @param {object} config the element configuration
-    * @param {object} dynamicForm the DynamicForm instance
+    * @param {FieldConfiguration} config the element configuration
+    * @param {DynamicForm} dynamicForm the DynamicForm instance
     * @async
     */
-    constructor(config, dynamicForm) {
+    constructor(config: FieldConfiguration, dynamicForm: DynamicForm) {
         this.config = config;
         // Repairing config file if parameters are missing (to write code easily)
         this.io = this.config.io ?? {};
@@ -43,16 +46,17 @@ class DynamicElement {
 
         let event = this.io.event ?? DynamicElement.defaultConfig.io.event;
 
-        this.htmlElement = dynamicForm.htmlElement.querySelectorAll(`[name="${config.name}"]`);
-        this.name = this.htmlElement[0].name;
+        this.htmlElement = dynamicForm.htmlElement.querySelectorAll(`[name="${config.name}"]`); // TODO: make the query only in DynamicForm
+        // this.name = this.htmlElement[0].name;
+        this.name = config.name;
         if (this.htmlElement.length === 0) {
             throw new Error(`Element ${config.name} not found`);
         } else if (this.htmlElement.length === 1) {
-            this.htmlElement = this.htmlElement[0];
-            this.htmlElement.addEventListener(event, (e) => { dynamicForm.notify(e.target.name); });
+            // this.htmlElement = this.htmlElement[0];
+            this.htmlElement[0].addEventListener(event, (e) => { dynamicForm.notify((e.target as HTMLInputElement).name); });
         } else {
             this.htmlElement.forEach(current => {
-                current.addEventListener(event, (e) => { dynamicForm.notify(e.target.name); });
+                current.addEventListener(event, (e) => { dynamicForm.notify((e.target as HTMLInputElement).name); });
             });
         }
     }
@@ -61,68 +65,64 @@ class DynamicElement {
     * Method to get the field value
     * @returns {string} the value
     */
-    get() {
+    public get(): any {
         // Custom
         if (this.io.get) {
             return this.io.get(this.htmlElement);
         }
         // Standard
-        return this.htmlElement.value;
+        return (this.htmlElement[0] as HTMLInputElement).value;
     }
 
     /**
     * Method to set the field value
     * @param {string} value new value to set
     */
-    set(value) {
+    public set(value): void {
         // Custom
         if (this.io.set) {
             return this.io.set(this.htmlElement, value);
         }
         // Standard
-        return this.htmlElement.value = value;
+        return (this.htmlElement[0] as HTMLInputElement).value = value;
     }
 
     /**
     * Method to clear this element from its content
     */
-    clear() {
+    public clear(): void {
         // Custom
         if (this.behavior.clear) {
             return this.behavior.clear(this.htmlElement);
         }
         // Standard
-        this.htmlElement.value = "";
+        (this.htmlElement[0] as HTMLInputElement).value = "";
     }
 
     /**
     * Method which updates the element (observer) status after a change on the observed subject
-    * @param {JSON} data data useful to the element's status change
+    * @param {object} data data useful to the element's status change
     * @param {string} subjectName name of the changed subject
     *
     * @returns a Promise in fulfilled state when element status has been updated
     *
     * @async
     */
-    async update(data, subjectName) {
-        return Promise.resolve(() => {
-            return this.beforeUpdate(data, subjectName);
-        }).then(result => {
-            if (result !== false) {
-                return this.updateStatus(data, subjectName);
-            }
-        }).then(() => {
-            return this.afterUpdate(data, subjectName);
-        });
+    public async update(data: object, subjectName: string): Promise<void> {
+        const beforeUpdateResult = this.beforeUpdate(data, subjectName);
+        if (beforeUpdateResult !== false) {
+            this.updateStatus(data, subjectName);
+        }
+        this.afterUpdate(data, subjectName);
     }
 
     /**
      * Method executed before the status update. If it returns false, the update is aborted.
-     * @param {JSON} data data useful to the element's status change
+     * @param {object} data data useful to the element's status change
      * @param {string} subjectName name of the changed subject
      * @returns false to abort the update, true otherwise
      */
-    beforeUpdate(data, subjectName) {
+    protected beforeUpdate(data: object, subjectName: string): boolean {
         // Custom
         if (this.behavior.beforeUpdate) {
             return this.behavior.beforeUpdate(this.htmlElement, data, subjectName);
@@ -133,10 +133,10 @@ class DynamicElement {
 
     /**
      * Method to update the DynamicElement status.
-     * @param {JSON} data data useful to the element's status change
+     * @param {object} data data useful to the element's status change
      * @param {string} subjectName name of the changed subject
      */
-    updateStatus(data, subjectName) {
+    protected updateStatus(data: object, subjectName: string): void {
         // Custom
         if (this.behavior.updateStatus) {
             return this.behavior.updateStatus(this.htmlElement, data, subjectName);
@@ -145,10 +145,10 @@ class DynamicElement {
 
     /**
      * Method executed after the status update. It is executed also if the update is aborted.
-     * @param {JSON} data data useful to the element's status change
+     * @param {object} data data useful to the element's status change
      * @param {string} subjectName name of the changed subject
      */
-    afterUpdate(data, subjectName) {
+    protected afterUpdate(data: object, subjectName: string): boolean {
         // Custom
         if (this.behavior.afterUpdate) {
             return this.behavior.afterUpdate(this.htmlElement, data, subjectName);
