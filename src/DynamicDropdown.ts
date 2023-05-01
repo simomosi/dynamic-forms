b5c9067c8cba240095944d3d588f9df800492cdc
simomosi <37104045+simomosi@users.noreply.github.com>
2023-05-01 19:13:05 +0100

    Migrated DynamicDropdown to ts

--- src/DynamicDropdown.ts
+++ src/DynamicDropdown.ts
@@ -1,4 +1,6 @@
 import DynamicElement from './DynamicElement.js';
+import DynamicForm from './DynamicForm.js';
+import { DropdownConfiguration, DropdownDropdownConfiguration } from './FieldConfigurationTypes.js';
 
 /**
 * This class represents a dropdown field with dynamic content (like a standard html-select with dynamic options)
@@ -6,12 +8,12 @@ import DynamicElement from './DynamicElement.js';
 class DynamicDropdown extends DynamicElement {
 
     /** @param {string} method the http request method for the remote call (async update) */
-    method;
+    method: string;
 
-    /** @param {object} checkbox - property with checkbox related properties */
-    dropdown;
+    /** @param {object} dropdown - property with dropdown related properties */
+    dropdown: DropdownDropdownConfiguration;
 
-    /** @param {JSON} defaultConfig property with default configuration values */
+    /** @param {object} defaultConfig property with default configuration values */
     static defaultConfig = { // Todo: inherit superclass' defaultConfig
         'io': {
             'event': 'change',
@@ -30,26 +32,35 @@ class DynamicDropdown extends DynamicElement {
     * @param {object} dynamicForm the DynamicForm instance
     * @async
     */
-    constructor(config, dynamicForm) {
+    constructor(config: DropdownConfiguration, dynamicForm: DynamicForm) {
         super(config, dynamicForm);
         this.method = this.fetch.method ?? DynamicDropdown.defaultConfig.fetch.method;
-        this.dropdown = this.config.dropdown ?? {};
+        this.dropdown = config.dropdown ?? {};
+    }
+
+
+    private getHtmlSelectElementOrList(): HTMLSelectElement | NodeList {
+        if (this.htmlElement.length === 1) {
+            return this.htmlElement[0] as HTMLSelectElement;
+        }
+        return this.htmlElement as NodeList;
     }
 
     /**
     * Method to clear the dropdown from all its non-empty options
     */
-    clear() {
+    public clear(): void {
         // Custom
         if (this.behavior.clear) {
-            return this.behavior.clear(this.htmlElement);
+            return this.behavior.clear(this.getHtmlSelectElementOrList());
         }
         // Standard
-        let options = this.htmlElement.getElementsByTagName('option');
+        const firstElement = this.htmlElement[0] as HTMLSelectElement;
+        let options = firstElement.getElementsByTagName('option');
         for (let i = options.length - 1; i >= 0; i--) {
             let value = options[i].value;
             if (value != null && value.trim() != '') { // Leave empty options
-                this.htmlElement.options[i] = null;
+                firstElement.options[i] = null;
             }
         }
     }
@@ -58,14 +69,14 @@ class DynamicDropdown extends DynamicElement {
      * Method executed before the status update. If it returns false, the update is aborted.
      * If dropdown.clearOnParentVoid is true and the subject value is empty, this method calls the clear function and aborts the update.
      *
-     * @param {JSON} data data useful to the element's status change
-     * @param {string} subjectName name of the changed subject
+     * @param {object} data data useful to the element's status change
+     * @param {string|null} subjectName name of the changed subject
      * @returns false to abort the update, true otherwise
      */
-    beforeUpdate(data, subjectName) {
+    protected beforeUpdate(data: object, subjectName: string|null): boolean {
         // Custom
         if (this.behavior.beforeUpdate) {
-            return this.behavior.beforeUpdate(this.htmlElement, data, subjectName);
+            return this.behavior.beforeUpdate(this.getHtmlSelectElementOrList(), data, subjectName);
         }
         // Standard
         if (subjectName && !data[subjectName]) { // Clear field on empty subject
@@ -80,12 +91,12 @@ class DynamicDropdown extends DynamicElement {
 
     /**
     * Method which execute a pipeline of instructions to update the select content with dynamic options
-    * @param {string} subjectName name of the subject who changed
-    * @param {JSON} data data to send with the http request
+    * @param {object} data data to send with the http request
+    * @param {string|null} subjectName name of the subject who changed
     *
     * @returns a Promise in fulfilled state when data has been updated
     */
-    updateStatus(data, subjectName) {
+    protected updateStatus(data: object, subjectName: string|null) {
         // Custom
         if (this.behavior.updateStatus) {
             return this.behavior.updateStatus(this.htmlElement, data, subjectName);
@@ -104,7 +115,6 @@ class DynamicDropdown extends DynamicElement {
             }
         }
 
-        let self = this;
         return fetch(requestUrl, fetchConfig)
             .then(response => { // Json
                 if (response.ok)
@@ -121,9 +131,9 @@ class DynamicDropdown extends DynamicElement {
 
     /**
      * Method to filter data returned by the remote call
-     * @param {JSON} data data to send with the http request
+     * @param {object} data data to send with the http request
      */
-    postProcessData(data) {
+    protected postProcessData(data: object[]): object[] {
         // Custom
         if (this.dropdown.postProcessData) {
             return this.dropdown.postProcessData(this.htmlElement, data);
@@ -134,32 +144,36 @@ class DynamicDropdown extends DynamicElement {
 
     /**
     * Method to save data returned by the remote call and filtered with postProcessData method
-    * @param {JSON} data data to send with the http request
+    * @param {object} data data to send with the http request
     *
     * @see postProcessData
     */
-    saveData(data) {
+    protected saveData(data: object[]): void {
         // Custom
         if (this.dropdown.saveData) {
             return this.dropdown.saveData(this.htmlElement, data);
         }
         // Standard
         this.clear();
-        // Add empty option
-        if (!this.htmlElement.querySelector('option:not([value]), option[value=""]')) {
-            let emptyOption = document.createElement("option");
-            emptyOption.text = '';
-            emptyOption.value = '';
-            this.htmlElement.add(emptyOption);
+        if (this.htmlElement.length == 1) {
+            // Add empty option
+            const firstElement = this.htmlElement[0] as HTMLSelectElement;
+            const emptyOption = firstElement.querySelector('option:not([value]), option[value=""]');
+            if (!emptyOption) {
+                let newEmptyOption = document.createElement("option");
+                newEmptyOption.text = '';
+                newEmptyOption.value = '';
+                firstElement.add(newEmptyOption);
+            }
+            // Add other options
+            data.forEach((item: {text: string, value: string}) => {
+                let option = document.createElement("option");
+                option.text = item.text;
+                option.value = item.value;
+                firstElement.add(option);
+            });
         }
-        // Add other options
-        data.forEach(item => {
-            let option = document.createElement("option");
-            option.text = item.text;
-            option.value = item.value;
-            this.htmlElement.add(option);
-        });
-        return data;
+        return;
     }
 
 }
